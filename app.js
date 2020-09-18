9f0d2f7935632cbddeaec00085168c0ea87fb7f3
santiBernstein <santiago.bernstein@gmail.com>
2020-09-17 21:30:01 -0300

    clase html

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3030, () => console.log('Server running in 3030 port'));
+
+
+app.get('/', function(req,res){
+    let file = path.resolve('vistas/index.html')
+    res.sendFile(file)
+});
+
+
+app.get('*', function (req,res){
+    if (req.url.endsWith('.css')) {
+        let file = path.resolve('public/styles' + req.url)
+        return res.sendFile(file)
+    }
+
+    let images = ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'svg']
+    let ext = req.url.split('.')[1]
+
+    if (images.includes(ext)){
+        let file = path.resolve('public/images' + req.url)
+        return res.sendFile(file)
+    }
+})    
